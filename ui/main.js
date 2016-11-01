c2df14e80226361407a80ec027fd8656b3f5fdf2
deepacorn97 <deepacorn97@gmail.com>
2016-11-01 13:49:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,47 +1,3 @@
-//counter code
-var button = document.getElementById('search');
-var counter = 0;
-
-button.search = function(){
-    
-    // create a request object
-    var request = newXMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function(){
-        if(request.readystate === XMLHttpRequest.DONE){
-           // Take some action
-           if(request.status === 200){
-               var counter = request.responseText;
-               var span = document.getElementById('count');
-               span.InnerHTML = counter.toString();
-           }
-        }
-        //Not done yet
-    };
-   //Make the request
-   request.open('GET','http://deepacorn97.imad.hasura-app.io/counter', true);
-   request.send(null);
-};
-
-// submit name
-var nameInput = document.getElementById("name");
-var name = nameInput.value;
-var submit = document.getElementById("submit_btn");
-submit.onclick = function(){
-    //make a request to the server and send the name
-    
-    //capture a list of name and render it as a list
-var names = ['name1','name2','name3','name4'];
-var list ='';
-for(var i=0; i<names.length; i++){
-    list += '<li>'+ names[i] +'</li>';
-}
-var ul = document.getElementById('namelist');
-ul.innerHTML=list;
-};
-
-
 console.log('Loaded!');
 
 // change the text of the main-text div
@@ -57,5 +13,3 @@ function moveRight(){
 img.onclick = function(){
     varinterval = setInterval(moveRight,50);
 };
-}
-}
