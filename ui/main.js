3bc7eda77a0e45fe67280e4bb170b3f4c7cb9413
deepacorn97 <deepacorn97@gmail.com>
2016-11-01 12:10:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,14 +4,24 @@ var counter = 0;
 
 button.search = function(){
     
-    //Make  a request to the counter endpoint
+    // create a request object
+    var request = newXMLHttpRequest();
     
     //Capture the response and store it in a variable
-    
-    //Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('search');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+           // Take some action
+           if(request.status === 200){
+               var counter = request.responseText;
+               var span = document.getElementById('count');
+               span.InnerHTML = counter.toString();
+           }
+        }
+        //Not done yet
+    };
+   //Make the request
+   request.open('GET','http://deepacorn97.imad.hasura-app.io/counter', true);
+   request.send(null);
 };
 
 // submit name
@@ -20,11 +30,12 @@ var name = nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick = function(){
     //make a request to the server and send the name
+    
     //capture a list of name and render it as a list
-var names = ["name1","name2","name3","name4"];
+var names = ['name1','name2','name3','name4'];
 var list ='';
 for(var i=0; i<names.length; i++){
-    list += '<li>'+names[i]+'</li>';
+    list += '<li>'+ names[i] +'</li>';
 }
 var ul = document.getElementById('namelist');
 ul.innerHTML=list;
