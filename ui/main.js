02077b05073c625225d1f252e8a07b3d14b1481d
deepacorn97 <deepacorn97@gmail.com>
2016-11-01 13:41:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,3 +40,22 @@ for(var i=0; i<names.length; i++){
 var ul = document.getElementById('namelist');
 ul.innerHTML=list;
 };
+
+
+console.log('Loaded!');
+
+// change the text of the main-text div
+var element = document.getElementById('main-text');
+element.innerHTML = 'New value';
+//move the image
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft+'px';
+}
+img.onclick = function(){
+    varinterval = setInterval(moveRight,50);
+};
+}
+}
