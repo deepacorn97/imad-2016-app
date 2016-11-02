f0d01bfb01d5fd4322f8c06f0d940e477f1bb3b7
deepacorn97 <deepacorn97@gmail.com>
2016-11-02 13:35:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,5 +11,5 @@ function moveRight(){
     img.style.marginLeft = marginLeft+'px';
 }
 img.onclick = function(){
-    varinterval = setInterval(moveRight,50);
+    varinterval = setInterval(moveRight,80);
 };
